e2da930ea3141f6deb435fa8be49e6f175859e83
My Huyen <myhuyen032023@gmail.com>
2024-09-02 09:36:17 +0700

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Hoang Thi My Huyen on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
